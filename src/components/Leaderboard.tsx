f5218c349d7ac26847ac19c581fac847be45f0f5
Austin-Spriggs <aspriggs@madisoncollege.edu>
2026-03-04 02:43:14 -0600

    leaderboard info loaded from GameResults file now displayed on Leaderboard.tsx in a table

--- src/components/Leaderboard.tsx
+++ src/components/Leaderboard.tsx
@@ -1,17 +1,55 @@
+import type { LeaderboardEntry } from "../functions/GameResults";
+
 /*
 	Leaderboard Props
 */
 type LeaderboardProps = {
-
+	leaderboard: LeaderboardEntry[]
+	player: string
 }
 
 /*
 	leaderboard Component
 */
-export const Leaderboard: React.FC<LeaderboardProps> = () => {
+export const Leaderboard: React.FC<LeaderboardProps> = ({ leaderboard, player }) => {
+
 	return (
 		<>
-			<h1>Leaderboard</h1>
+			<div className="overflow-x-auto rounded-box border border-base-content/5 bg-base-100 w-96 mx-auto mt-5">
+				<table className="table">
+					{/* head */}
+					<thead>
+						<tr>
+							<th className="w-5">#</th>
+							<th>Name</th>
+							<th className="text-center">Wins</th>
+							<th className="text-center">Losses</th>
+							<th className="text-center">Ratio</th>
+						</tr>
+					</thead>
+					<tbody>
+						{
+							leaderboard.map((p, i) =>
+								<tr key={ p.name }>
+									<td>{ i + 1 }</td>
+									<td className=" flex items-center gap-5 w-25">
+										{ p.name }
+										{ p.name == player
+											? <span className="material-symbols-outlined text-purple-900">person_pin</span>
+											: null
+										}
+									</td>
+									<td className="text-right">{ p.wins }</td>
+									<td className="text-right">{ p.losses }</td>
+									<td className="text-right">{ p.ratio }</td>
+								</tr>
+							)
+						}
+					</tbody>
+				</table>
+			</div>
+
+			<div className="dockSpacer pb-15"></div>
 		</>
 	);
 }
