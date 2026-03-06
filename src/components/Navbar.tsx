10b7004eddebaf6cff2b466032de6335d9e95589
Austin Spriggs <aspriggs@madisoncollege.edu>
2026-03-06 14:59:04 -0600

    moved navigation into the navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,30 +1,64 @@
-import { useLocation } from "react-router";
+import { useNavigate, useLocation } from "react-router";
+
 
 type NavbarProps = {
 	playerName: string
 }
 
 export const Navbar: React.FC<NavbarProps> = ({ playerName }) => {
+	const nav = useNavigate();
 	const location = useLocation();
 	const path = location.pathname;
 
 	let page: string;
 
 	switch (path) {
-		case '/':				page = 'My Stats';			break;
-		case '/setup':			page = 'Setup';				break;
-		case '/play':			page = 'Play';				break;
-		case '/leaderboard':	page = 'Leaderboard';		break;
-		default:				page = 'Page Not Found';	break;
+		case '/': page = 'My Stats'; break;
+		case '/setup': page = 'Setup'; break;
+		case '/play': page = 'Play'; break;
+		case '/leaderboard': page = 'Leaderboard'; break;
+		default: page = 'Page Not Found'; break;
 	}
 
 	return (
 		<>
-			{/* Top navbar for aesthetic */}
 			<div className="navbar font-[Fira_Sans] bg-purple-900 text-white shadow-md">
 				<h1 className="text-xl pl-2 font-bold navbar-start">MLEM</h1>
-				<h1 className="text-2xl font-bold navbar-center">{ page }</h1>
-				<div className="text-sm pr-2 navbar-end">{ playerName }</div>
+				<h1 className="text-2xl font-bold navbar-center">{page}</h1>
+
+				<div className="text-sm pr-2 navbar-end">
+					<div className="drawer drawer-end">
+						<input id="my-drawer-1" type="checkbox" className="drawer-toggle" />
+
+						<div className="grid grid-flow-col items-center justify-items-end">
+							<label htmlFor="my-drawer-1" className="material-symbols-outlined drawer-button">dehaze</label>
+						</div>
+
+						<div className="drawer-side text-black">
+							<label htmlFor="my-drawer-1" aria-label="close sidebar" className="drawer-overlay"></label>
+							<ul className="menu bg-base-200 min-h-full w-80 p-4">
+								{/* Sidebar content here */}
+								<li className={ path == '/' ? 'bg-purple-900 text-white rounded-sm' : '' }
+									onClick={() => nav('/')}
+								>
+									<a>My Stats</a>
+								</li>
+
+								<li className={ path == '/play' ? 'bg-purple-900 text-white rounded-sm' : '' }
+									onClick={() => nav('/play')}
+								>
+									<a>Play</a>
+								</li>
+
+								<li className={ path == '/leaderboard' ? 'bg-purple-900 text-white rounded-sm' : '' }
+									onClick={() => nav('/leaderboard')}
+								>
+									<a>Leaderboard</a>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
 			</div>
 		</>
 	)
