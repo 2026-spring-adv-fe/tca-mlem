e6733c611e076fe044dd491fee324b5a660c5154
Austin Spriggs <aspriggs@madisoncollege.edu>
2026-03-06 20:21:57 -0600

    button to abort a game

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -42,41 +42,46 @@ export const Navbar: React.FC<NavbarProps> = ({ player }) => {
 				<h1 className="text-2xl font-bold navbar-center">{page}</h1>
 
 				<div className="text-sm pr-2 navbar-end">
-					<div className="drawer drawer-end">
-						<input id="navDrawer" ref={ drawerRef } type="checkbox" className="drawer-toggle" />
+					{ path == '/play'
+						? <button className="btn bg-red-600 border-red-600 text-white" onClick={ () => nav('/') } >
+							Abort Game
+						</button>
+						: <div className="drawer drawer-end">
+							<input id="navDrawer" ref={ drawerRef } type="checkbox" className="drawer-toggle" />
 
-						<div className="grid grid-flow-col items-center justify-items-end">
-							<label htmlFor="navDrawer" className="material-symbols-outlined drawer-button">dehaze</label>
+							<div className="grid grid-flow-col items-center justify-items-end">
+								<label htmlFor="navDrawer" className="material-symbols-outlined drawer-button">dehaze</label>
+							</div>
+
+							<div className="drawer-side text-black">
+								<label htmlFor="navDrawer" aria-label="close sidebar" className="drawer-overlay"></label>
+								<ul className="menu bg-base-200 min-h-full w-60 p-4">
+									{/* Sidebar content here */}
+									<li className="text-end">Hello, { player }!</li>
+
+									<div className="divider my-1"></div>
+
+									<li className={ path == '/' ? 'bg-purple-900 text-white rounded-sm' : '' }
+										onClick={() => go('/') }
+									>
+										<a>My Stats</a>
+									</li>
+
+									<li className={ path == '/setup' ? 'bg-purple-900 text-white rounded-sm' : '' }
+										onClick={() => go('/setup') }
+									>
+										<a>Play</a>
+									</li>
+
+									<li className={ path == '/leaderboard' ? 'bg-purple-900 text-white rounded-sm' : '' }
+										onClick={() => go('/leaderboard') }
+									>
+										<a>Leaderboard</a>
+									</li>
+								</ul>
+							</div>
 						</div>
-
-						<div className="drawer-side text-black">
-							<label htmlFor="navDrawer" aria-label="close sidebar" className="drawer-overlay"></label>
-							<ul className="menu bg-base-200 min-h-full w-60 p-4">
-								{/* Sidebar content here */}
-								<li className="text-end">Hello, { player }!</li>
-
-								<div className="divider my-1"></div>
-
-								<li className={ path == '/' ? 'bg-purple-900 text-white rounded-sm' : '' }
-									onClick={() => go('/') }
-								>
-									<a>My Stats</a>
-								</li>
-
-								<li className={ path == '/play' ? 'bg-purple-900 text-white rounded-sm' : '' }
-									onClick={() => go('/play') }
-								>
-									<a>Play</a>
-								</li>
-
-								<li className={ path == '/leaderboard' ? 'bg-purple-900 text-white rounded-sm' : '' }
-									onClick={() => go('/leaderboard') }
-								>
-									<a>Leaderboard</a>
-								</li>
-							</ul>
-						</div>
-					</div>
+					}
 				</div>
 			</div>
 		</>
