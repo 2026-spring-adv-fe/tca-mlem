433d775c6ca8542ce4f5329db8eae60ca8fedb62
Austin Spriggs <aspriggs@madisoncollege.edu>
2026-03-06 20:29:06 -0600

    extrapolated the navbar drawer to its own component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,5 @@
+import { Drawer } from "./Drawer";
+
 import { useNavigate, useLocation } from "react-router";
 import { useRef } from "react";
 
@@ -46,41 +48,7 @@ export const Navbar: React.FC<NavbarProps> = ({ player }) => {
 						? <button className="btn bg-red-600 border-red-600 text-white" onClick={ () => nav('/') } >
 							Abort Game
 						</button>
-						: <div className="drawer drawer-end">
-							<input id="navDrawer" ref={ drawerRef } type="checkbox" className="drawer-toggle" />
-
-							<div className="grid grid-flow-col items-center justify-items-end">
-								<label htmlFor="navDrawer" className="material-symbols-outlined drawer-button">dehaze</label>
-							</div>
-
-							<div className="drawer-side text-black">
-								<label htmlFor="navDrawer" aria-label="close sidebar" className="drawer-overlay"></label>
-								<ul className="menu bg-base-200 min-h-full w-60 p-4">
-									{/* Sidebar content here */}
-									<li className="text-end">Hello, { player }!</li>
-
-									<div className="divider my-1"></div>
-
-									<li className={ path == '/' ? 'bg-purple-900 text-white rounded-sm' : '' }
-										onClick={() => go('/') }
-									>
-										<a>My Stats</a>
-									</li>
-
-									<li className={ path == '/setup' ? 'bg-purple-900 text-white rounded-sm' : '' }
-										onClick={() => go('/setup') }
-									>
-										<a>Play</a>
-									</li>
-
-									<li className={ path == '/leaderboard' ? 'bg-purple-900 text-white rounded-sm' : '' }
-										onClick={() => go('/leaderboard') }
-									>
-										<a>Leaderboard</a>
-									</li>
-								</ul>
-							</div>
-						</div>
+						: <Drawer player={ player } path={ path } />
 					}
 				</div>
 			</div>
