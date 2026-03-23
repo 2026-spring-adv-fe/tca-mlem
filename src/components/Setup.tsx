f754b9ca536977d854202228ba2ad9bedd42380e
Austin Spriggs <aspriggs@madisoncollege.edu>
2026-03-23 13:03:15 -0500

    added some logic for a dummy setup screen

--- src/components/Setup.tsx
+++ src/components/Setup.tsx
@@ -1,22 +1,63 @@
+// React
 import { useNavigate } from "react-router";
+import { useState } from "react";
 
+// NPM
+import clsx from "clsx";
+
+// Types
+import type { Player } from "./Play";
+
+
+type Players = {
+	player: Player,
+}
 
 /*
 	Setup Props
 */
 type SetupProps = {
-
+	player: string,
+	setPlayers: (players: Player[]) => void,
 }
 
 /*
 	Setup Component
 */
-export const Setup: React.FC<SetupProps> = () => {
+export const Setup: React.FC<SetupProps> = ({ player, setPlayers }) => {
 	const nav = useNavigate();
+
+	const [setupPage, setSetupPage] = useState(1);
+	const [setupComplete, setSetupComplete] = useState(true);
+
+	const updatePlayers = () => {
+		setPlayers([
+			{ name: 'Austin', page: 1 },
+			{ name: 'Avery', page: 2 },
+		]);
+
+		nav('/play');
+	}
+
 	return (
 		<>
-			<h1>Setup</h1>
-			<button className="btn btn-primary btn-outline" onClick={ () => nav('/play') }>Play Game</button>
+		<div className="w-96 text-center mt-5 mx-auto">
+			<p className={ setupPage != 1 ? 'hidden' : ''}>How many people are playing?</p>
+			<select defaultValue="" className={clsx("select w-50 mt-2", setupPage != 1 && 'hidden')} onChange={ (e) => console.log(e.target.value) }>
+				<option disabled={true}>1</option>
+				<option>2</option>
+				<option>3</option>
+				<option>4</option>
+				<option>5</option>
+			</select>
+		</div>
+
+		<div className="controls text-center">
+			{ setupComplete
+				? <button className="btn btn-primary btn-outline" onClick={ updatePlayers }>Play Game</button>
+				: <button className="btn bg-purple-800 mx-auto my-5" onClick={ () => setSetupPage(setupPage + 1) }>Next</button>
+			}
+		</div>
 		</>
 	);
 }
