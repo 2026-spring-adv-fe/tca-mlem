5694f9c30aeb0812cba0cd81fb8302b921d50ffe
Austin Spriggs <aspriggs@madisoncollege.edu>
2026-03-30 12:59:52 -0500

    creates list of available players on setup screen

--- src/components/Setup.tsx
+++ src/components/Setup.tsx
@@ -11,15 +11,16 @@ import clsx from "clsx";
 import type { Player } from "./Play";
 
 
-type Players = {
-	player: Player,
+type AvailablePlayer = {
+	name: string,
+	checked: boolean,
 }
 
 /*
 	Setup Props
 */
 type SetupProps = {
-	player: string,
+	playerName: string,
 	allPlayers: string[],
 	setCurrentPlayers: (players: Player[]) => void,
 }
@@ -29,39 +30,59 @@ type SetupProps = {
 
 	TODO: Checklist of all previous players? (Get Players from gameResults.players)
 */
-export const Setup: React.FC<SetupProps> = ({ player, allPlayers, setCurrentPlayers }) => {
+export const Setup: React.FC<SetupProps> = ({ playerName, allPlayers, setCurrentPlayers }) => {
 	const nav = useNavigate();
 
-	console.log(allPlayers);
 
 	const [setupPage, setSetupPage] = useState(1);
 	const [setupComplete, setSetupComplete] = useState(true);
+	const [availablePlayers, setAvailablePlayers] = useState(allPlayers.map(p =>
+		({
+			name: p,
+			checked: p === playerName ? true : false,
+		})
+	).sort((a, b) =>
+		// Sort results so the current user is at the top
+		(a.checked ? -1 : 0) - (b.checked ? -1 : 0)
+	));
 
-	const updateCurrentPlayers = () => {
-		setCurrentPlayers([
-			{ name: 'Snape', page: 1 },
-			{ name: 'Hermione', page: 2 },
-		]);
+	// Instead of [currentUser, setCurrentUser], you could do -
+	// const currentPlayersStateTuple = useState<string[]>([]);
 
-		nav('/play');
+	const updateAvailablePlayers = (player: AvailablePlayer) => {
+		setAvailablePlayers(availablePlayers.map(p =>
+			({
+				name: p.name,
+				checked: p.name == player.name
+					? !p.checked
+					: p.checked
+			})
+		));
+
+		// nav('/play');
+
 	}
 
 	return (
 		<>
-		<div className="w-96 text-center mt-5 mx-auto">
-			<p className={ setupPage != 1 ? 'hidden' : ''}>How many people are playing?</p>
-			<select defaultValue="" className={clsx("select w-50 mt-2", setupPage != 1 && 'hidden')} onChange={ (e) => console.log(e.target.value) }>
-				<option disabled={true}>1</option>
-				<option>2</option>
-				<option>3</option>
-				<option>4</option>
-				<option>5</option>
-			</select>
+		<div className="w-96 mt-5 mx-auto">
+			Select Players:
+			{ availablePlayers.map(p =>
+				<label className="block" key={ p.name }>
+					<input type="checkbox"
+						className="checkbox mx-2"
+						onChange={ () => updateAvailablePlayers(p) }
+						checked={ p.checked }
+						disabled={ p.name == playerName }
+					/>
+					{ p.name }
+				</label>
+			)}
 		</div>
 
 		<div className="controls text-center">
 			{ setupComplete
-				? <button className="btn btn-primary btn-outline" onClick={ updateCurrentPlayers }>Play Game</button>
+				? <button className="btn btn-primary btn-outline" onClick={ () => nav('/play') }>Play Game</button>
 				: <button className="btn bg-purple-800 mx-auto my-5" onClick={ () => setSetupPage(setupPage + 1) }>Next</button>
 			}
 		</div>
