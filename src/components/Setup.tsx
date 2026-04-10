fe132f703d628eb9951bd40f87f7fbe5da52e89f
Austin Spriggs <aspriggs@madisoncollege.edu>
2026-04-09 19:39:51 -0500

    allows user to add players during setup

--- src/components/Setup.tsx
+++ src/components/Setup.tsx
@@ -22,6 +22,7 @@ type AvailablePlayer = {
 type SetupProps = {
 	playerName: string,
 	allPlayers: string[],
+	currentPlayers: Player[],
 	setCurrentPlayers: (players: Player[]) => void,
 }
 
@@ -30,24 +31,45 @@ type SetupProps = {
 
 	TODO: Checklist of all previous players? (Get Players from gameResults.players)
 */
-export const Setup: React.FC<SetupProps> = ({ playerName, allPlayers, setCurrentPlayers }) => {
+export const Setup: React.FC<SetupProps> = ({ playerName, allPlayers, currentPlayers, setCurrentPlayers }) => {
 	const nav = useNavigate();
 
+	/*
+		For sorting availablePlayers in a way that the current player is first
 
+		Abstracted into function since functionality is needed in a few places
+	*/
+	const sortAvailablePlayers = (availablePlayers: AvailablePlayer[]) => {
+		return [...availablePlayers].sort((a, b) => (a.checked ? -1 : 0) - (b.checked ? -1 : 0));
+	}
+
+	/*
+		Hooks
+	*/
 	const [setupPage, setSetupPage] = useState(1);
-	const [setupComplete, setSetupComplete] = useState(true);
-	const [availablePlayers, setAvailablePlayers] = useState(allPlayers.map(p =>
-		({
-			name: p,
-			checked: p === playerName ? true : false,
-		})
-	).sort((a, b) =>
-		// Sort results so the current user is at the top
-		(a.checked ? -1 : 0) - (b.checked ? -1 : 0)
+	const [setupComplete, setSetupComplete] = useState(false);
+	const [newPlayer, setNewPlayer] = useState('');
+	const [availablePlayers, setAvailablePlayers] = useState(sortAvailablePlayers(
+		allPlayers.map(p =>
+			({
+				name: p,
+				checked: p === playerName ? true : false,
+			})
+		)
 	));
 
 	// Set current players when available players is updated
 	useEffect(() => {
+		// Add current player if not in the database yet
+		if (!availablePlayers.find(p => p.name == playerName)) {
+			setAvailablePlayers(
+				sortAvailablePlayers([
+					...availablePlayers,
+					{ name: playerName, checked: true },
+				])
+			);
+		}
+
 		// Get the names of selected players
 		const players = availablePlayers.filter(p =>
 			p.checked
@@ -60,14 +82,15 @@ export const Setup: React.FC<SetupProps> = ({ playerName, allPlayers, setCurrent
 			({
 				name: p,
 				cat: 'Chef',
-
 				page: i += 1,
 			})
 		));
 	}, [availablePlayers]);
 
 
-	// Updates available players when a new player is selected
+	/*
+		Updates available players when a new player is selected
+	*/
 	const updateAvailablePlayers = (player: AvailablePlayer) => {
 		setAvailablePlayers(availablePlayers.map(p =>
 			({
@@ -79,32 +102,82 @@ export const Setup: React.FC<SetupProps> = ({ playerName, allPlayers, setCurrent
 		));
 
 		// nav('/play');
-
 	}
+
+	const addPlayer = () => {
+		const player = newPlayer.trim();
+
+		// Return if input is empty or player already exists
+		if (!player) return;
+		if (availablePlayers.find(p => p.name.toLowerCase() == player.toLowerCase())) return;
+
+		setAvailablePlayers([
+			...availablePlayers,
+			{ name: player, checked: true }
+		]);
+
+		// Empty input field
+		setNewPlayer('');
+	}
+
 
 	return (
 		<>
-		<div className="w-96 mt-5 mx-auto">
-			Select Players:
-			{ availablePlayers.map(p =>
-				<label className="block" key={ p.name }>
-					<input type="checkbox"
-						className="checkbox mx-2"
-						onChange={ () => updateAvailablePlayers(p) }
-						checked={ p.checked }
-						disabled={ p.name == playerName }
-					/>
-					{ p.name }
-				</label>
-			)}
-		</div>
+		{ setupPage == 1
+			? <>
+				<form className="w-96 mt-5 mx-auto flex items-center"
+					onSubmit={ (e) => {
+						// Prevent page reload
+						e.preventDefault();
 
+						// Add the new player
+						addPlayer();
+					}}
+				>
+					<input
+						type="text"
+						className="input w-80 mr-2 mt-2"
+						value={ newPlayer }
+						onChange={ (e) => setNewPlayer(e.target.value)}
+						placeholder="Add a new player"
+					/>
+
+					<button
+						type="submit"
+						className="material-symbols-outlined btn bg-purple-800 text-white mt-2"
+					>
+						add
+					</button>
+				</form>
+
+				<div className="w-96 mt-5 mx-auto">
+					Select Players:
+					{ availablePlayers.map(p =>
+						<label className="block mt-2" key={ p.name }>
+							<input type="checkbox"
+								className="checkbox mx-2"
+								onChange={ () => updateAvailablePlayers(p) }
+								checked={ p.checked }
+								disabled={ p.name == playerName }
+							/>
+							{ p.name }
+						</label>
+					)}
+				</div>
+			</>
+
+			: <>
+				{ currentPla}
+			</>
+		}
 		<div className="controls text-center">
 			{ setupComplete
-				? <button className="btn btn-primary btn-outline" onClick={ () => nav('/play') }>Play Game</button>
+				? <button className="btn bg-purple-800" onClick={ () => nav('/play') }>Play Game</button>
 				: <button className="btn bg-purple-800 mx-auto my-5" onClick={ () => setSetupPage(setupPage + 1) }>Next</button>
 			}
 		</div>
+
+
 		</>
 	);
 }
